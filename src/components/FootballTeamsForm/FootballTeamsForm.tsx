f07ff06c93364ccd221507dabb8ca61b380593fc
BartlomiejJaruga <245827@edu.p.lodz.pl>
2026-01-18 15:57:51 +0100

    feat: - Connected FootballTeamsForm loading teams list with backend - Implemented axiosInstance and proxy to backend

--- src/components/FootballTeamsForm/FootballTeamsForm.tsx
+++ src/components/FootballTeamsForm/FootballTeamsForm.tsx
@@ -3,25 +3,26 @@ import styles from './FootballTeamsForm.module.scss';
 import type { TeamsListResponseDTO } from '@dtos/TeamsListResponseDTO';
 import { useEffect, useState } from 'react';
 import { sortListAlphabetically } from '@lib/utils';
-
-const TEAMS: TeamsListResponseDTO = {
-  teams_list: [
-    'England',
-    'Poland',
-    'Germany',
-    'Italy',
-    'France',
-    'Spain',
-    'Russia',
-  ],
-};
+import axiosInstance from '@services/axiosInstance';
 
 export function FootballTeamsForm() {
   const [teamList, setTeamList] = useState<string[]>([]);
 
   useEffect(() => {
-    const sortedTeams = sortListAlphabetically(TEAMS.teams_list);
-    setTeamList(sortedTeams);
+    const loadTeams = async () => {
+      try {
+        const response = await axiosInstance.get('/v1/teams');
+
+        const teams = response.data as TeamsListResponseDTO;
+
+        const sortedTeams = sortListAlphabetically(teams.teams_list || []);
+        setTeamList(sortedTeams);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    loadTeams();
   }, []);
 
   return (
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,33 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+
+const BACKEND_ADDRESS = 'https://82d8e4b1dc44.ngrok-free.app';
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: BACKEND_ADDRESS,
+        changeOrigin: true,
+        configure: (proxy) => {
+          proxy.on('proxyReq', (proxyReq) => {
+            proxyReq.setHeader('Origin', BACKEND_ADDRESS);
+          });
+        },
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
-      "@components": path.resolve(__dirname, "src/components"),
-      "@styles": path.resolve(__dirname, "src/styles"),
-      "@assets": path.resolve(__dirname, "src/assets"),
-      "@lib": path.resolve(__dirname, "src/lib"),
-      "@dtos": path.resolve(__dirname, "scr/services/dtos"),
-      "@services": path.resolve(__dirname, "src/services"),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@lib': path.resolve(__dirname, 'src/lib'),
+      '@dtos': path.resolve(__dirname, 'scr/services/dtos'),
+      '@services': path.resolve(__dirname, 'src/services'),
     },
   },
   css: {
